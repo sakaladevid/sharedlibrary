a5f6b95792279eb95451d34644ac96dd1d3c278b
sakaladevid <152602442+sakaladevid@users.noreply.github.com>
2023-12-21 11:01:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,6 +7,13 @@ def ContBuild()
       sh 'mvn package'
 }
 
+def ContDeploy(jobname,ip,appname)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${Context}.war"
+}
+
+
+
 
 
 
